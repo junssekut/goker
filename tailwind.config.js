b3b234cf7e3a4a6ca2f32f9e44c24d85308a712b
matthewlya <matthewraditya09@gmail.com>
2024-11-28 23:05:07 +0700

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'footer' : '#101820'
+            }
         },
     },
     plugins: [],
