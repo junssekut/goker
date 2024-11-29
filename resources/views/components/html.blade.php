c6ecdcb400f539466f2424eff18851955100a98e
sheanfinnegan <sheanfinnegan2905@gmail.com>
2024-11-29 13:27:36 +0700

    navbar fix

--- resources/views/components/html.blade.php
+++ resources/views/components/html.blade.php
@@ -5,12 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>GoKer | Berkarir Bersama Gojek</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
+    <link rel="icon" href="{{ asset('assets/images/goker-icon.png') }}" type="image/x-icon">
 
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
@@ -909,10 +910,49 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script defer>
+        document.addEventListener("DOMContentLoaded", function() {
+            let nav = document.querySelector('.navbar');
+
+
+
+            function handleScroll() {
+                if (window.scrollY > 10) {
+                    nav.classList.add("bg-white", "pt-0", "shadow-md");
+                    nav.classList.remove("bg-transparent", "pt-7", "shadow-none");
+                } else {
+                    nav.classList.add("bg-transparent", "pt-7", "shadow-none");
+                    nav.classList.remove("bg-white", "pt-0", "shadow-md");
+                }
+            }
+
+
+            if (window.innerWidth > 1124) {
+                window.addEventListener("scroll", handleScroll);
+                nav.classList.remove("bg-white", "pt-0", "shadow-md");
+                nav.classList.add("bg-transparent", "pt-7", "shadow-none");
+            } else {
+                window.removeEventListener("scroll", handleScroll);
+                nav.classList.add("bg-white", "pt-0", "shadow-md");
+                nav.classList.remove("bg-transparent", "pt-7", "shadow-none");
+            }
+
+
+            window.addEventListener("resize", function() {
+                if (window.innerWidth > 1124) {
+                    window.addEventListener("scroll", handleScroll);
+                    nav.classList.remove("bg-white", "pt-0", "shadow-md");
+                    nav.classList.add("bg-transparent", "pt-7", "shadow-none");
+                } else {
+                    window.removeEventListener("scroll", handleScroll);
+                    nav.classList.add("bg-white", "pt-0", "shadow-md");
+                    nav.classList.remove("bg-transparent", "pt-7", "shadow-none");
+                }
+            });
+        });
+
         function DropDown(e) {
             let list = document.querySelector('.nav-right');
-            let nav = document.querySelector('.navbar');
-            console.log(list)
+
             if (e.name === 'menu') {
                 e.name = "close";
                 list.classList.remove('top-[-200px]');
@@ -920,18 +960,17 @@
                 list.classList.add('opacity-100');
             } else {
                 e.name = "menu";
-                list.classList.remove('top-[80px]'); // Remove the class for 80px
-                list.classList.add('top-[-200px]'); // Add back the old class
+                list.classList.remove('top-[80px]');
+                list.classList.add('top-[-200px]');
                 list.classList.remove('opacity-100');
             }
 
 
         }
     </script>
-
     <style>
         * {
-            z-index: 0;
+            z-index: 100;
         }
     </style>
 
--- resources/views/components/nav-before.blade.php
+++ resources/views/components/nav-before.blade.php
@@ -1,6 +1,6 @@
 <nav
-    class="navbar justify-around md:flex md:items-center w-full md:px-44 md:justify-between bg-white rounded-bl-2xl rounded-br-2xl shadow-md
-            h-24 md:h-auto fixed z-10
+    class="navbar justify-around md:flex md:items-center w-full md:px-44 md:justify-between rounded-bl-2xl rounded-br-2xl
+            h-24 md:h-auto fixed pt-7 bg-transparent backdrop-blur-0 transition-all duration-700 shadow-none
             ">
     <div class="flex justify-between items-center md:flex-none">
         <span class="h-24 flex items-center pl-5 md:pl-0">
@@ -15,8 +15,8 @@
 
     <div
         class="nav-right md:flex md:justify-around md:gap-16 md:opacity-100 opacity-0 
-            top-[-200px] transition-all ease-in duration-500 bg-white md:static absolute
-            w-full md:w-auto z-[0] md:z-auto pt-7 md:pt-0 rounded-bl-2xl rounded-br-2xl md:rounded-b-0">
+            top-[-200px] transition-all ease-in duration-500  md:static absolute z-0
+            w-full md:w-auto md:z-auto pt-7 md:pt-0 rounded-bl-2xl rounded-br-2xl md:rounded-b-0 bg-white md:bg-transparent">
         <ul class="md:flex md:items-center md:justify-between md:gap-14">
             <div class="w-full flex justify-center">
                 <a href="#" class="nav-list font-britHeavy text-lg">Beranda</a>
