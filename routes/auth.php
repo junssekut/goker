74264e1637922f4ac8af6b22941e615ab56f20d0
Irene Setievi <setieviirene@gmail.com>
2024-12-11 11:14:02 +0700

    'login_hrd'

--- routes/auth.php
+++ routes/auth.php
@@ -11,6 +11,9 @@ Route::middleware('guest')->group(function () {
 
     Volt::route('login', 'pages.auth.login')
         ->name('login');
+    
+     Volt::route('loginHrd', 'login-hrd')
+        ->name('loginHrd');
 
     Volt::route('forgot-password', 'pages.auth.forgot-password')
         ->name('password.request');
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\LoginHrdForm;
 use Livewire\Volt\Volt;
+
 
 Route::view('/', 'home')->name('home');
 Route::view('/register', 'register')->name('register');
 Route::view('/login', 'login')->name('login');
 Route::view('/dev', 'login')->name('dev');
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
