6d80cbf4d4a069def98239cc92f32e708a5431ec
junskii <114347889+junssekut@users.noreply.github.com>
2025-03-05 08:31:12 +0700

    third party login (with google)

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->stateless()->user();
+            $user = User::where('email', $googleUser->getEmail())->first();
+
+            if ($user) {
+                Auth::login($user);
+            } else {
+                $user = User::create([
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'password' => bcrypt(str_random(16)), // or any random password
+                    'google_id' => $googleUser->getId(),
+                ]);
+                Auth::login($user);
+            }
+
+            return redirect()->route('home');
+        } catch (\Exception $e) {
+            return redirect()->route('login');
+        }
+    }
+}
